c73565ac51429a7b0f086126db7298882f6c6261
bbs <91828379+shin0624@users.noreply.github.com>
2023-04-08 00:06:20 +0900

    Rectangle 클래스 만들기 ver.2(위임생성자 + 멤버변수 초기화)

---
+++ glegle.h
@@ -0,0 +1,9 @@
+class Rectangle {
+public:
+	int width;
+	int height;
+	Rectangle();
+	Rectangle(int w, int h);
+	Rectangle(int length);
+	bool isSquare();
+};
--- 소스.cpp
+++ 소스.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
 using namespace std;
 
-#include "헤더.h"
+#include "glegle.h"
 
-int Rectangle::getArea() {
-	return width * height;
+Rectangle::Rectangle():Rectangle(1) {}//위임생성자
+
+Rectangle::Rectangle(int w, int h) {//타겟생성자
+	width = w, height = h;
+}
+
+Rectangle::Rectangle(int length) {
+	width = height = length;
+}
+
+bool Rectangle::isSquare() {
+	if (width == height) return true;//정사각형이면 true를 리턴하는 메서드
+	else
+		return false;
 }
 
 int main() {
-	Rectangle rect;
-	cout << "width";
-	cin >> rect.width;
-	
-	cout << "height";
-	cin >> rect.height;
+	Rectangle rect1;
+	Rectangle rect2(3, 5);
+	Rectangle rect3(3);
 
-	
-	cout << "사각형의 면적은 " << rect.getArea() << endl;
+	if (rect1.isSquare()) cout << "rect1은 정사각형이다." << endl;
+	if (rect2.isSquare()) cout << "rect2은 정사각형이다." << endl;
+	if (rect3.isSquare()) cout << "rect3은 정사각형이다." << endl;
 }
